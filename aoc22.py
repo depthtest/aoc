e8257805d7d5667f90a7ec1ca72120eca3274b77
Jesus Ojeda <615452+depthtest@users.noreply.github.com>
2022-12-02 17:09:51 +0100

    aoc22 day 2

--- aoc22.py
+++ aoc22.py
@@ -5,7 +5,7 @@ from heapq import heapify, heappush, heappop
 def parse_day1():
     deers = []
     curr_deer = []
-    with open('input', 'r') as ff:
+    with open('input.txt', 'r') as ff:
         for line in ff:
             if line == "\n":
                 deers.append(curr_deer)
@@ -23,6 +23,54 @@ def day1p1():
 def day1p2():
     deers = parse_day1()
     print(sum(list(sorted(map(lambda x: sum(x), deers))[-3:])))
+
+def parse_day2():
+    plays = []
+    with open('input') as ff:
+        for line in ff:
+            other, me = line.strip().split(' ')
+            plays.append((other, me))
+    return plays
+
+def day2p1():
+    mapper = {
+        'A':'R', 'B':'P', 'C':'S',
+        'X':'R', 'Y':'P', 'Z':'S',
+    }
+    score_mapper = {'R': 1,'P': 2,'S': 3,}
+    win = {('R','P'), ('P','S'), ('S', 'R')}
+    los = {('R','S'), ('P','R'), ('S', 'P')}
+
+    plays = parse_day2()
+    acc_score = 0
+    for play in map(lambda y: tuple(map(lambda x: mapper[x], y)), plays):
+        if play in win:
+            acc_score += 6 + score_mapper[play[1]]
+        elif play in los:
+            acc_score += 0 + score_mapper[play[1]]
+        else:
+            acc_score += 3 + score_mapper[play[1]]
+    print(acc_score)
+
+def day2p2():
+    mapper = {
+        'A':'R', 'B':'P', 'C':'S',
+        'X':'L', 'Y':'T', 'Z':'W',
+    }
+    score_mapper = {'R': 1, 'P': 2, 'S': 3}
+    win = {'R':'P', 'P':'S', 'S':'R'}
+    los = {'R':'S', 'P':'R', 'S':'P'}
+
+    plays = parse_day2()
+    acc_score = 0
+    for play in map(lambda y: tuple(map(lambda x: mapper[x], y)), plays):
+        if play[1] == 'W':
+            acc_score += 6 + score_mapper[win[play[0]]]
+        elif play[1] == 'L':
+            acc_score += 0 + score_mapper[los[play[0]]]
+        else:
+            acc_score += 3 + score_mapper[play[0]]
+    print(acc_score)
 
 import sys
 eval('day' + sys.argv[1] + '()')
